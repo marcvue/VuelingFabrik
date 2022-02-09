f05d0896f7dab847e4a361d2995a9c6fca46554c
Marc <Marc@DESKTOP-LV4N0MR>
2022-02-09 19:00:30 +0100

    Agregar archivos de proyecto.

---
+++ VuelingFabrik/Domain/Subcomponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingFabrik.Domain
+{
+    public abstract class Subcomponent
+    {
+        public abstract int GetPrice();
+    }
+}
---
+++ VuelingFabrik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VuelingFabrik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection();
+
+        }
+    }
+}
